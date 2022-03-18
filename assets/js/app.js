cb9df470c6d55bed3b13428161f3fe268cbbbd6f
Misuch <abujaico@estudiantes.crackthecode.la>
2022-03-17 21:10:17 -0600

    vista de la contraseña

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,26 +13,26 @@ sign_in_btn.addEventListener("click", () => {
 
 
 //Contraseñas 
-var Eye = document.getElementById("#Eye");
-var input = document.getElementById("#pass-new");
-Eye.addEventListener("click", () => {
-        if (input.type === "password") {
-            input.type = "text";
-            Eye.style.opacity = 0.8;
-        } else {
-            input.type = "password";
-            Eye.style.opacity = 0.2;
-        }
-    })
-
-var ojo = document.getElementById("#ojo");
-var input = document.getElementById("#pass-ojo");
-ojo.addEventListener("click", function () {
+var eye = document.getElementById('Eye');
+var input = document.getElementById('Input');
+eye.addEventListener("click", function(){
     if (input.type === "password") {
         input.type = "text";
-        ojo.style.opacity = 0.8;
+        eye.style.opacity = 0.8;
     } else {
         input.type = "password";
+        eye.style.opacity = 0.2;
+    }
+})
+
+var ojo = document.getElementById('Ojo');
+var pass = document.getElementById('Input-pass');
+ojo.addEventListener("click", function(){
+    if (pass.type === "password") {
+        pass.type = "text";
+        ojo.style.opacity = 0.8;
+    } else {
+        pass.type = "password";
         ojo.style.opacity = 0.2;
     }
 })
